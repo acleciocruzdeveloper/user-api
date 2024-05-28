f267ecc59cc6e65996f2f033e546ff2c52caa88c
acleciocruz <aclecioscruz@gmail.com>
2024-05-27 23:49:30 -0300

    implements orm customers and address application, configure migration flyway

---
+++ src/main/java/br/com/backend/userapi/repositories/CustomersRepository.java
@@ -0,0 +1,11 @@
+package br.com.backend.userapi.repositories;
+
+import br.com.backend.userapi.domain.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomersRepository extends JpaRepository<Customers, Long> {
+
+    Optional<Customers> getUserByCpf(String cpf);
+}
