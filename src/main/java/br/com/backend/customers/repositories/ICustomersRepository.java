bfa07dfc5b0b0a5bc4f0dfe885e5309723c7272b
acleciocruz <aclecioscruz@gmail.com>
2024-05-30 21:37:28 -0300

    implements search by name customers

--- src/main/java/br/com/backend/customers/repositories/ICustomersRepository.java
+++ src/main/java/br/com/backend/customers/repositories/ICustomersRepository.java
@@ -3,6 +3,7 @@ package br.com.backend.customers.repositories;
 import br.com.backend.customers.domain.Customers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ICustomersRepository extends JpaRepository<Customers, Long> {
@@ -10,4 +11,6 @@ public interface ICustomersRepository extends JpaRepository<Customers, Long> {
     Optional<Customers> findByCpf(String cpf);
 
     boolean existsByEmail(String email);
+
+    List<Customers> queryByNome(String nome);
 }
